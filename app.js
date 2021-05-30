c46b1519cc680785effe4edae8b750ebfdf6aad2
h.kawai <hironori86@gmail.com>
2021-05-30 18:34:40 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-'use strict';
+"use strict";
+function fib(n) {
+  if (n === 0) {
+      console.log(n +'番目の0です');
+    return 0;
+  } else if (n === 1) {
+      console.log(n + '番目の1です');
+    return 1;
+  }
+  return fib(n -1) + fib(n - 2);
+}
+const length = 5;
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+}
+
+
